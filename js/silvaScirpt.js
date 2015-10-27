afc2472ae020a8c282961bec07c061ff60b3bcdc
silva <silvaamr93@gmail.com>
2015-10-27 03:39:42 +0200

    Update silvaScirpt.js

--- js/silvaScirpt.js
+++ js/silvaScirpt.js
@@ -1,1 +1,10 @@
-$(document).ready(function(){"use strict";var b=($("#intro"),$("#intro img"));$(".col span").click(function(){var a=$(".one");$("#contentshow").slideToggle(1200,function(){a.fadeIn()})}),$("#contentshow button").click(function(){$("#contentshow").slideUp(500),one.fadeOut()}),b.fadeIn(3e3,function(){$("#intro p").fadeIn(2e3)}),$(".col").hover(function(){$(".col span").toggleClass("hover")}),$(".col span").hover(function(){$(this).toggleClass("hover")})});
+$(document).ready(function(){"use strict";
+$(window).load(function(){
+  $(body).addClass("flow");
+  
+  
+  
+});
+
+
+var b=($("#intro"),$("#intro img"));$(".col span").click(function(){var a=$(".one");$("#contentshow").slideToggle(1200,function(){a.fadeIn()})}),$("#contentshow button").click(function(){$("#contentshow").slideUp(500),one.fadeOut()}),b.fadeIn(3e3,function(){$("#intro p").fadeIn(2e3)}),$(".col").hover(function(){$(".col span").toggleClass("hover")}),$(".col span").hover(function(){$(this).toggleClass("hover")})});
